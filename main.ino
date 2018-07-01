9ca83732562e0434e0c1ebeec23600ecb2133688
Brian Scott Simon <zutroy97@yahoo.com>
2018-06-30 23:00:40 -0500

    Before StatusInfo_t

--- main.ino
+++ main.ino
@@ -10,13 +10,13 @@
 #define ONE_WIRE_BUS 5
 const uint8_t MAX_TEMPERATURE_SENSORS = 2;
 const int TEMPERATURE_INTERVAL = 5;
+const int STRING_BUFFER_SIZE = 20;
 typedef uint8_t DeviceAddress[8];
-
+char buffer[STRING_BUFFER_SIZE];
 typedef struct
 {
     DeviceAddress Address;
-    float LastTemp;
-    char Name[17];
+    int LastTempInHundredths;
 } TempInfo_t;
 
 TempInfo_t tempInfo[MAX_TEMPERATURE_SENSORS] = { 
@@ -25,11 +25,10 @@ TempInfo_t tempInfo[MAX_TEMPERATURE_SENSORS] = {
  };
 
 uint8_t temperatureProbeCount = 0;
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature DS1820(&oneWire);
-
 unsigned long lastTemperatureSent = 0;
 
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS1820(&oneWire);
 
 HomieNode temperatureNode("temperature", "temperature");
 
@@ -47,21 +46,17 @@ void addressToString(DeviceAddress addr, char* result)
 }
 
 void setupHandler(){
-    //char addr[17];
-    
     temperatureNode.setProperty("unit").send("f");
     for(uint8_t x = 0; x < temperatureProbeCount; x++)
     {
-        //addressToString(tempInfo[x].Address, addr);
-        temperatureNode.advertise(tempInfo[x].Name);
+        addressToString(tempInfo[x].Address, buffer);
+        temperatureNode.advertise(buffer);
     }
-    //temperatureNode.advertise("degrees");
 }
 
 void loopHandler(){
     float tempF;
     static bool isWaiting;
-    char addr[17];
     //Homie.getLogger() << "loopHandler() "  << endl;
     if (millis() - lastTemperatureSent >= TEMPERATURE_INTERVAL * 1000UL || lastTemperatureSent == 0) {
         isWaiting = true;
@@ -75,9 +70,10 @@ void loopHandler(){
         for(uint8_t x = 0; x < temperatureProbeCount; x++)
         {
             tempF = DS1820.getTempF(tempInfo[x].Address);
-            addressToString(tempInfo[x].Address, addr);
-            temperatureNode.setProperty(addr).send(String(tempF));
-            tempInfo[x].LastTemp = tempF;
+            tempInfo[x].LastTempInHundredths = (int)(tempF * 100);
+            addressToString(tempInfo[x].Address, buffer);
+            temperatureNode.setProperty(buffer).send(String(tempF));
+            
         }
     }
 }
@@ -85,58 +81,25 @@ void loopHandler(){
 void setupTemperatureProbes()
 {
     DeviceAddress addr;
-    char name[17];
+
     temperatureProbeCount = DS1820.getDS18Count();
     temperatureProbeCount = min(temperatureProbeCount, MAX_TEMPERATURE_SENSORS);
     for (uint8_t x = 0; x < temperatureProbeCount; x++)
     {
         if (DS1820.getAddress(addr, x)){
             memcpy(tempInfo[x].Address, addr, 8);
-            tempInfo[x].LastTemp = 0.0;
-            addressToString(tempInfo[x].Address, name);
-            memcpy(tempInfo[x].Name, name, 17);
+            tempInfo[x].LastTempInHundredths = 0;
         }
     }
-    //DS1820.requestTemperatures(); 
 }
-
 
 // function to print a device address
 void printAddress(DeviceAddress deviceAddress)
 {
+    addressToString(deviceAddress, buffer);
+    Serial << buffer; 
+}
 
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    // zero pad the address if necessary
-    if (deviceAddress[i] < 16) Serial.print("0");
-    Serial.print(deviceAddress[i], HEX);
-  }
-}
-/*
-void loopHandlerTest(){
-    float tempF;
-    static bool isWaiting;
-    //Homie.getLogger() << "loopHandler() "  << endl;
-    if (millis() - lastTemperatureSent >= TEMPERATURE_INTERVAL * 1000UL || lastTemperatureSent == 0) {
-        isWaiting = true;
-        DS1820.requestTemperatures(); 
-        Homie.getLogger() << "DS1820.requestTemperatures(): "  << endl;
-        lastTemperatureSent = millis();
-    }
-    if (isWaiting & DS1820.isConversionComplete())
-    {
-        isWaiting = false;
-        for(uint8_t x = 0; x < temperatureProbeCount; x++)
-        {
-            tempF = DS1820.getTempF(tempInfo[x].Address);
-            Serial << x << ") ";
-            printAddress(tempInfo[x].Address);
-            Serial << " " << tempF << " F" << endl;
-            tempInfo[x].LastTemp = tempF;
-        }
-    }
-}
-*/
 void setup()
 {
     Serial.begin(115200);
@@ -155,11 +118,4 @@ void setup()
 void loop()
 {
     Homie.loop();
-    //loopHandlerTest();
-    /*
-    char addr[17];
-    addressToString(tempInfo[0].Address, addr);
-    Serial.println(addr);
-*/
-    
 }
